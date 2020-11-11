d41c282b33f181f530eafc38d1ae699eebaca3f9
Sophia Elize Cruz <secruz@hawaii.edu>
2020-11-10 16:38:33 -1000

    fixed link to landing

--- app/imports/ui/pages/MapView.jsx
+++ app/imports/ui/pages/MapView.jsx
@@ -1,41 +1,8 @@
 import React from 'react';
 import { Grid, Button, Image, Form } from 'semantic-ui-react';
-import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-
-/** Create a schema to specify the structure of the data to appear in the form. */
-const formSchema = new SimpleSchema({
-    name: String,
-    quantity: Number,
-    condition: {
-        type: String,
-        allowedValues: ['excellent', 'good', 'fair', 'poor'],
-        defaultValue: 'good',
-    },
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
 
 /** Renders the Page for adding a document. */
 class MapView extends React.Component {
-
-    /** On submit, insert the data. */
-    submit(data, formRef) {
-        const { name, quantity, condition } = data;
-        const owner = Meteor.user().username;
-        Stuffs.collection.insert({ name, quantity, condition, owner },
-            (error) => {
-                if (error) {
-                    swal('Error', error.message, 'error');
-                } else {
-                    swal('Success', 'Item added successfully', 'success');
-                    formRef.reset();
-                }
-            });
-    }
-
     /** IMAGES AS TEMP PLACEHOLDERS FOR MAPS */
     render() {
         return (
